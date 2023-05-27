a0130cc4b09283572aa6161686a46531f307ad4f
christopherflores9312 <christopherflores9312@gmail.com>
2023-05-27 13:11:56 -0700

    fixed dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
-// import dependencies
-const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 
-// create connection to our database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
-  port: 3306,
+  port: process.env.DB_PORT,
 });
 
 module.exports = sequelize;
