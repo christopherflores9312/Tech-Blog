b1b3f6dc080c051d58d9d480ce003494d4cf4d8f
christopherflores9312 <christopherflores9312@gmail.com>
2023-05-27 11:45:30 -0700

    starter files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers'); // assuming you have an index.js file in your controllers directory that exports your routes
+const sequelize = require('./config/connection'); // assuming you have a connection.js file in a config directory
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SESSION_SECRET, // this should be set in your .env file
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })); // assuming you have a main.handlebars file in your layouts directory
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
