f9017e0aa5b33ae07c9fa7f04baa3893d16f0a70
christopherflores9312 <christopherflores9312@gmail.com>
2023-05-30 23:30:49 -0700

    Connection.js modified

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize');
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
