45a850634b54ae56faa5b8b529c6256cd31fa596
christopherflores9312 <christopherflores9312@gmail.com>
2023-05-30 23:45:41 -0700

    connection.js mod

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
-
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
 let sequelize;
 
 if (process.env.CLEARDB_DATABASE_URL) {
-  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
